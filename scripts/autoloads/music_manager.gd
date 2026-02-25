c5f664579c2dac71e491784c1e5f282e67975946
siyangsun <siyangsun2007@gmail.com>
2026-02-24 23:36:26 -0500

    death screen music

--- scripts/autoloads/music_manager.gd
+++ scripts/autoloads/music_manager.gd
@@ -2,6 +2,7 @@ extends Node
 
 var _menu_player: AudioStreamPlayer
 var _game_player: AudioStreamPlayer
+var _death_player: AudioStreamPlayer
 
 
 func _ready() -> void:
@@ -17,22 +18,31 @@ func _ready() -> void:
 	_game_player.stream = game_stream
 	add_child(_game_player)
 
+	var death_stream := load("res://assets/audio/deathscreen.mp3") as AudioStreamMP3
+	death_stream.loop = true
+	_death_player = AudioStreamPlayer.new()
+	_death_player.stream = death_stream
+	add_child(_death_player)
+
 	GameManager.state_changed.connect(_on_state_changed)
 	_on_state_changed(GameManager.state)
 
 
 func _on_state_changed(new_state: GameManager.State) -> void:
 	match new_state:
-		GameManager.State.MENU, GameManager.State.DEAD:
-			_switch_to(_menu_player, _game_player)
+		GameManager.State.MENU:
+			_switch_to(_menu_player, [_game_player, _death_player])
+		GameManager.State.DEAD:
+			_switch_to(_death_player, [_menu_player, _game_player])
 		GameManager.State.PLAYING:
-			_switch_to(_game_player, _menu_player)
+			_switch_to(_game_player, [_menu_player, _death_player])
 		GameManager.State.PAUSED:
 			pass  # keep whatever is playing
 
 
-func _switch_to(start: AudioStreamPlayer, stop: AudioStreamPlayer) -> void:
+func _switch_to(start: AudioStreamPlayer, stop: Array) -> void:
 	if start.playing:
 		return
-	stop.stop()
+	for p in stop:
+		p.stop()
 	start.play()
